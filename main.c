d6aabb8d2cf1a34a376606a8567a9d05032d0589
ludovic sutter <sutterludovic67@gmail.com>
2020-07-04 10:07:23 +0000

    suite

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main()
 
 void game_loop(t_game * g)
 {
+  int p_hand = ;
   
   print_player_coins(g->player);
   broker_new_hand(g);
@@ -19,7 +20,21 @@ void game_loop(t_game * g)
   print_player_hand(g->player);
   broker_place_bet(g);
   player_place_bet(g);
-  sum_player_hand(g);
-  int p_hand
-  print_turn_results(g, p_hand);
+ 
+  player_ask_cards(g);
+  sum_player_hand(g->player);
+  
+  if (p_hand>21)
+  {
+    player_lost(g);
+  }
+  if (p_hand == 21)
+  {
+    player_win(g);
+  }
+  if (p_hand<21 && inhand ==6)
+  {
+    player_win(g);
+  }
+ print_turn_results(g, p_hand);
 }
