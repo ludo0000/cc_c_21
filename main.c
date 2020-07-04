0e27676d520814b6a1294dbeb7c1d285e2e7900f
ludovic sutter <sutterludovic67@gmail.com>
2020-07-04 14:58:21 +0000

    fini

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 
 int p_hand;
+int score;
+int p;
 
 int main()
 {
@@ -18,17 +20,16 @@ void game_loop(t_game * g)
 
   int i;
   i = 0;
-  while ( 1 >=0 && i <= 5 )
+  while ( g->player->score >=0 && i <= 5 )
   {
-    
+   printf("\nC'est un nouveau tour qui commence, on en est au : %i e\n",i);
    print_player_coins(g->player);
    broker_new_hand(g);
    player_new_hand(g);
    print_player_hand(g->player);
    broker_place_bet(g);
    player_place_bet(g);
-   player_ask_cards(g);
-   sum_player_hand(g->player);
+   p = player_ask_cards(g);
    p_hand = sum_player_hand(g->player);
    print_turn_results(g, p_hand);
    
@@ -51,16 +52,20 @@ void game_loop(t_game * g)
    {
     player_lost(g); 
    }
-   if (player_ask_cards(g) ==6 && p_hand<21)
+   if (p ==6 && p_hand<21)
    {
      player_win(g);
    }
-   
    i++;
+   
   }
-  printf ("La partie est finie");
+  printf ("La partie est finie\n");
   if (i == 5)
   {
-    printf (" Tu as gagné, t'es si fort.");
+    printf (" Tu as gagné, t'es si fort.\n Tu as gagné %i coins", g->player->score );
+  }
+  else
+  {
+    printf("Tu as perdu !");
   }
 }
